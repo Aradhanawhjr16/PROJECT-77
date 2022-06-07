4d3054585ec1a65576da4a4518b03bf0c95d32e2
Aradhanawhjr16 <95398267+Aradhanawhjr16@users.noreply.github.com>
2022-06-07 16:05:42 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+var first_name = "Aradhana"
+var second_name = "Samuel"
+var full_name = first_name.concat(second_name);
+console.log(full_name);
+var a = 10;
+var b = 8 ;
+var add = a + b ;
+console.log(add);
+var c = 20;
+var d = 10;
+var sub = c - d ;
+console.log(sub);
+var e = 2 ;
+var f = 5 ;
+var multiply = e*f;
+console.log(multiply);
+var g = 6 ;
+var h = 2;
+var divide = g/h;
+console.log(divide);
